3523fb39b31d4c8e940cadc0c87a1c3c834d2fb4
sg-object <sg.object@gmail.com>
2025-08-25 11:17:23 +0900

    First Commit

---
+++ validation/src/test/java/com/sg/boot/validation/ValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.boot.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
